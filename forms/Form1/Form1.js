55f73d7e8ade7892206a7826bde1ba35893de0bb
MaciYelaska <maciyelaska@creighton.edu>
2021-09-15 14:15:02 -0500

    Added first and last name code

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,3 +1,9 @@
 // this is the original first version 
 
 console.log(`This is a test for the tutorial demo.`)
+
+// new code Version 2
+let myFirstName = prompt("Enter your first name: ")
+console.log(`Your first name is ${myFirstName}.`)
+let myLastName = prompt("Enter your last name: ")
+console.log(`Your first name is ${myLastName}.`)
